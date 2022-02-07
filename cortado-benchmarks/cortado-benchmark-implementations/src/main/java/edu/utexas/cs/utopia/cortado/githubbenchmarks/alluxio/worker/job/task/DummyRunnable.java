73ea170042bd9684b067bbb7bbd636dd63cb0998
Benjamin Sepanski <ben_sepanski@utexas.edu>
2022-02-07 17:41:10 -0600

    Add DummyRunnable so CountableThreadPool compiles

---
+++ cortado-benchmarks/cortado-benchmark-implementations/src/main/java/edu/utexas/cs/utopia/cortado/githubbenchmarks/alluxio/worker/job/task/DummyRunnable.java
@@ -0,0 +1,10 @@
+package edu.utexas.cs.utopia.cortado.githubbenchmarks.alluxio.worker.job.task;
+
+public class DummyRunnable implements Runnable
+{
+    @Override
+    public void run()
+    {
+
+    }
+}
